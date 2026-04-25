cbe34209d32d50b9c1482e03234274cfac21670e
emmanuelgodknowz-lab <emmanuelgodknowz@gmail.com>
2026-04-25 17:35:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const VERIFY_TOKEN = "test123";
+const VERIFY_TOKEN = "ogbodomaduabuchi18";
 
 app.use(express.json());
 
